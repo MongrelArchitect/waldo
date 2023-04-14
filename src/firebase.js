8b04ab4b2dc4a931e84c32013ce05fb696d829aa
Sean Eric Thomas <set@seanericthomas.com>
2023-04-14 12:52:26 -0700

    Link project with firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCoYXvOQM0yEZJj0MfHuh9OCVgv0g1_pCw',
+  authDomain: 'waldo-c3546.firebaseapp.com',
+  projectId: 'waldo-c3546',
+  storageBucket: 'waldo-c3546.appspot.com',
+  messagingSenderId: '686227823033',
+  appId: '1:686227823033:web:a777085c6934c13a463e68',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
