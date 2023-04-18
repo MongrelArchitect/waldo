952d38f3acfa304a0a0c795fd2a9eae2c6be4aa9
Sean Eric Thomas <set@seanericthomas.com>
2023-04-18 13:41:59 -0700

    Gather assets and add Header to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import beastie from './images/beastie.jpg';
+import creeper from './images/creeper.jpg';
+import superStar from './images/super-star.jpg';
 
 export default function App() {
+  const [characters, setCharacters] = useState([
+    {
+      name: 'beastie',
+      img: beastie,
+      found: false,
+    },
+    {
+      name: 'creeper',
+      img: creeper,
+      found: false,
+    },
+    {
+      name: 'super star',
+      img: superStar,
+      found: false,
+    },
+  ]);
+
   return (
-    <div />
+    <div>
+      <Header characters={characters} setCharacters={setCharacters} />
+    </div>
   );
 }
