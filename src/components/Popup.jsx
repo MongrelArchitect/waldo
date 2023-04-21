484946612ba299e54b5d1e180250d6f64eab8baf
Sean Eric Thomas <set@seanericthomas.com>
2023-04-20 18:22:08 -0700

    Check clicked character against database

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import db from '../helpers/firebase';
 
 export default function Popup({
-  characters, checkCoords, coords, visible,
+  characters,
+  // eslint-disable-next-line
+  checkCoords,
+  coords,
+  visible,
 }) {
+  // Place the popup window based on mouse cursor
   const style = {
     left: coords.x,
     top: coords.y,
+  };
+
+  const handleClick = async (event) => {
+    // Query database to see if we've found the character
+    const { character } = event.target.dataset;
+    const querySnapshot = await getDocs(collection(db, 'characters'));
+    querySnapshot.forEach((doc) => {
+      if (doc.id === character) {
+        // Get the coordinate range of the character we've selected
+        const { xMax } = doc.data();
+        const { xMin } = doc.data();
+        const { yMax } = doc.data();
+        const { yMin } = doc.data();
+        if (
+          // Check if we've clicked the right place
+          checkCoords.x >= xMin
+          && checkCoords.x <= xMax
+          && checkCoords.y >= yMin
+          && checkCoords.y <= yMax
+        ) {
+          // Found 'em!
+          console.log('found \'em!');
+          // XXX TODO
+          // now what?
+        }
+      }
+    });
   };
 
   return (
@@ -19,7 +53,12 @@ export default function Popup({
           return null;
         }
         return (
-          <span key={character.name}>
+          // eslint-disable-next-line
+          <span
+            key={character.name}
+            onClick={handleClick}
+            data-character={character.name}
+          >
             <img src={character.img} alt={character.name} />
             {character.name}
           </span>
