035c40123d15e58f97b0819c6214dbde5d372e7e
Sean Eric Thomas <set@seanericthomas.com>
2023-04-24 12:47:48 -0700

    Add character names to "Home" and tweak styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,14 @@ import Characters from './Characters';
 
 export default function Home({ characters }) {
   return (
-    <div>
+    <div className="home">
       <h1>Find &apos;Em</h1>
       <div className="instructions">
-        <span>How to play:</span>
-
+        <h2>How to play:</h2>
         <span>
           Your goal is to find several characters hidden within a chaotic scene,
           just like the classic puzzle book series
+          {' '}
           <a
             target="_blank"
             href="https://en.wikipedia.org/wiki/Where%27s_Wally%3F"
@@ -25,10 +25,14 @@ export default function Home({ characters }) {
           possible, and compare your score to others on the scoreboard. Will you
           be the fastest? Let&apos;s go!
         </span>
-
-        <Characters characters={characters} />
-        <NavLink to="/play">Play!</NavLink>
       </div>
+      <Characters characters={characters} />
+      <div className="char-names">
+        {characters.map((character) => (
+          <span key={character.name}>{character.name}</span>
+        ))}
+      </div>
+      <NavLink to="/play" className="play-link">START</NavLink>
     </div>
   );
 }
